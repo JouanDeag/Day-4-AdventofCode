3f7060e1fee24a8eb690848437a0d609b635a437
JouanDeag <61161558+JouanDeag@users.noreply.github.com>
2021-12-07 21:31:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-const input = require('fs').readFileSync('data.txt', 'utf8').split('\r\n');
+const input = require('fs').readFileSync('data.txt', 'utf8').split('\r\n\r\n');
+const opened = new Set();
 
+let instruction = input[0];
+input.shift();
+let brickor = input;
+
+instruction = instruction.split(',').map(e => parseInt(e, 10));
+brickor = brickor.map(bricka => bricka.split('\r\n').map(row => row.split(" ").filter(cell => cell.length > 0).map(cell => parseInt(cell, 10))))
+
+console.log();
+
+for (let i = 0; i < instruction.length; i++) {
+    opened.add(instruction[i]);
+}
+
+function areyaWinninSon (bricka) {
+    bricka.forEach(row => { 
+        row.forEach(cell => { 
+            opened.has(cell);
+        })  
+    })
+}
